07ee980345b0ecac679438caf5477a6f49fbb384
Dylan Avery <fossilfuelmusic@gmail.com>
2016-12-15 14:47:11 -0700

    momentjs stuff

--- lib/components/MessageWindow.jsx
+++ lib/components/MessageWindow.jsx
@@ -4,7 +4,7 @@ const MessageWindow = ( { messages } ) => {
 
   return (
     <ul>
-      { messages.map(m => <li key={m.key}> {m.user.displayName}: {m.content} </li>) }
+      { messages.map(m => <li key={m.key}> {m.user.displayName}: {m.content} {m.createdAt}</li>) }
     </ul>
   );
 
